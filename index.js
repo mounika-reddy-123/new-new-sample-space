f04c5a696e21998211e3ade64acfdc26c74ac94a
mounika-reddy-123 <lakshmimounika.kakumuri@gmail.com>
2023-12-31 09:34:17 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express=require("express")
 const app=express()
+const cors=require('cors')
+app.use(cors())
+app.use(express.json())
+const {homeDetails}=require("./homeDetails.json")
 
 app.get('/',(req,res)=>{
     res.send("Welcome to my Browser")
 })
 
-app.listen(3000,()=>{
-    console.log("server started at 3000//")
+app.get('/list',(req,res)=>{
+    res.send(homeDetails)
+})
+
+app.listen(4000,()=>{
+    console.log("Server started at 4000//")
 })
